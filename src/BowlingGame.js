c6d00c7827737d6238621cf363ffc248c88fec23
CarQuispe <carmen.quispe@ucb.edu.bo>
2023-09-25 00:49:42 -0400

    deveria ser un juego con todos 1

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -1,10 +1,23 @@
 class Game{
-  roll(pins){
+
+  constructor(){
+    this.rolls = new Array(21).fill(0);
+    this.currentRoll = 0;
     
   }
+  roll(pins){
+    this.rolls[this.currentRoll++]=pins;
+    //this.getScore += pins;
+  }
   getScore(){
-    return 0;
+    let getScore =0;
+    let i =0;
+    for(let frame =0; frame<10;frame ++){
+      getScore += this.roll[i]+ this.rolls[i+1];
+      i+=2;
+    }
+    return getScore;
   }
 
 }
-export default Game;
+module.exports= Game;
--- src/BowlingGame.test.js
+++ src/BowlingGame.test.js
@@ -1,12 +1,25 @@
 import Game from "./BowlingGame.js";
 
 describe("Juego de bolos", () => {
-  it("deberia crear el objeto de juego", () => {
-    const g = new Game(); 
-    for(let i = 0; i<20;i++){
-      g.roll(0);
+  let g;
+
+  beforeEach(() => {
+    g = new Game();
+  });
+
+  function rollMany(n, pins) {
+    for (let i = 0; i < n; ++i) {
+      g.roll(pins);
     }
-    
+  }
+
+  it("deberia crear el objeto de juego", () => {
+    rollMany(20, 0);
     expect(g.getScore()).toBe(0);
+  });
+
+  it("deberia ser un juego con todos los tiros de valor 1", () => {
+    rollMany(20, 1);
+    expect(g.getScore()).toBe(20);
   });
 });
